4a55d5a3f5d7e100cccbf469557aac217e913d39
Gabriel Hoban <gabehoban1@posteo.de>
2023-11-25 22:40:08 -0500

    fix telegraf configuration

--- router-parts/service.sh
+++ router-parts/service.sh
@@ -13,12 +13,12 @@ set service ntp server 10.32.40.51
 set service ntp server 10.32.40.52
 
 # Telegraf Monitoring
-set service monitoring telegraf authentication organization $SECRET_TELEGRAF_ORG
-set service monitoring telegraf authentication token $SECERT_TELEGRAF_TOKEN
-set service monitoring telegraf bucket $SECERT_TELEGRAF_BUCKET
-set service monitoring telegraf port '8086'
+set service monitoring telegraf influxdb authentication organization $SECRET_TELEGRAF_ORG
+set service monitoring telegraf influxdb authentication token $SECERT_TELEGRAF_TOKEN
+set service monitoring telegraf influxdb bucket $SECERT_TELEGRAF_BUCKET
+set service monitoring telegraf influxdb port '8086'
+set service monitoring telegraf influxdb url $SECRET_TELEGRAF_HOST
 set service monitoring telegraf source 'all'
-set service monitoring telegraf url $SECRET_TELEGRAF_HOST
 
 # SSH server
 set service ssh disable-password-authentication
