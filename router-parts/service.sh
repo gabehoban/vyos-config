73da219dc80ee5541aef8125ebc91780e28f5754
Gabriel Hoban <gabehoban1@posteo.de>
2023-11-30 20:13:34 -0500

    Remove telegraf support

--- router-parts/service.sh
+++ router-parts/service.sh
@@ -12,14 +12,6 @@ delete service ntp server
 set service ntp server 10.32.40.51
 set service ntp server 10.32.40.52
 
-# Telegraf Monitoring
-set service monitoring telegraf influxdb authentication organization $SECRET_TELEGRAF_ORG
-set service monitoring telegraf influxdb authentication token $SECERT_TELEGRAF_TOKEN
-set service monitoring telegraf influxdb bucket $SECERT_TELEGRAF_BUCKET
-set service monitoring telegraf influxdb port '8086'
-set service monitoring telegraf influxdb url $SECRET_TELEGRAF_HOST
-set service monitoring telegraf source 'all'
-
 # SSH server
 set service ssh disable-password-authentication
 set service ssh listen-address '10.32.10.254'
