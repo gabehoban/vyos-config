2b38d534d154dac1bb26b8663becc017a7fc3df4
Gabriel Hoban <gabehoban1@posteo.de>
2023-12-03 09:13:54 -0500

    rotate wireguard keys

--- router-parts/interfaces.sh
+++ router-parts/interfaces.sh
@@ -22,11 +22,10 @@ set interfaces ethernet eth2 hw-id '00:05:1b:96:9d:0b'
 set interfaces wireless wlan0 physical-device 'phy0'
 set interfaces wireless wlan0 hw-id 'e4:a7:a0:2e:14:68'
 
-# Pubkey: NRrnGU46eGHEM7UgtLJxtp7SJ08pOSro3Y4ygJLGi3Y=
 set interfaces wireguard wg01 address '10.20.0.254/24'
 set interfaces wireguard wg01 description 'WIREGUARD'
 set interfaces wireguard wg01 peer iphone-gabe allowed-ips '10.20.0.21/32'
 set interfaces wireguard wg01 peer iphone-gabe persistent-keepalive '15'
-set interfaces wireguard wg01 peer iphone-gabe public-key 'thL0/u8iYnhE5KdIw3TsXnootMwuXhmcmBEByYbjoBs='
+set interfaces wireguard wg01 peer iphone-gabe public-key "${SECRET_WIREGUARD_CLIENT_0_PUBKEY}"
 set interfaces wireguard wg01 port '51820'
-set interfaces wireguard wg01 private-key "${SECRET_WIREGUARD_PRIVATE_KEY}"
+set interfaces wireguard wg01 private-key "${SECRET_WIREGUARD_PRIVKEY}"
