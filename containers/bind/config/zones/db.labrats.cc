cdf1a07977b7fa84d87ad33ff600784823e0debe
Gabriel Hoban <gabehoban1@posteo.de>
2023-12-14 22:08:58 -0500

    Fix typos

--- containers/bind/config/zones/db.labrats.cc
+++ containers/bind/config/zones/db.labrats.cc
@@ -5,7 +5,7 @@
 $TTL 3600
 $ORIGIN labrats.cc.
 @ 3600 IN SOA gateway.labrats.cc. gateway.labrats.cc. (
-    1702605044         ; serial number (epoch timestamp)
+    1702609704         ; serial number (epoch timestamp)
     7200               ; refresh period
     3600               ; retry period
     1209600            ; expire time
