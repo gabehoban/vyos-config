e66f7e7812c6f520be606f3430d726c75ddeb999
Gabriel Hoban <gabehoban@icloud.com>
2024-03-22 18:52:36 -0400

    Fix BIND9 configuration typo

--- router-parts/protocols.sh
+++ router-parts/protocols.sh
@@ -10,5 +10,5 @@ set protocols bgp neighbor 10.32.40.42 remote-as '64512'
 set protocols bgp neighbor 10.32.40.43 address-family ipv4-unicast
 set protocols bgp neighbor 10.32.40.43 description 'cluster-node-03'
 set protocols bgp neighbor 10.32.40.43 remote-as '64512'
-set protocols bgp parameters router-id '10.32.40.254'
+set protocols bgp parameters router-id '10.32.0.254'
 set protocols bgp system-as '64512'
